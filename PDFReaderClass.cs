a777af6406477c90cff4e4906c046a40891da8eb
Marcus Barbosa <marcus.barbosa.net@gmail.com>
2020-05-01 13:41:29 -0300

    Add project.

---
+++ PDFReaderClass.cs
@@ -0,0 +1,40 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfReaderEXE
+{
+    public class PdfReaderClass
+    {
+        public static string toString(string sPath)
+        {
+            using (PdfReader oPDF = new PdfReader(sPath))
+            {
+                StringBuilder oText = new StringBuilder();
+                for (int i = 1; i <= oPDF.NumberOfPages; i++)
+                {
+                    oText.Append(PdfTextExtractor.GetTextFromPage(oPDF, i));
+                }
+                return oText.ToString();
+            }
+        }
+
+        public static List<string> toList(string sPath)
+        {
+            using (PdfReader oPDF = new PdfReader(sPath))
+            {
+                var oText = new StringBuilder();
+                for (int i = 1; i <= oPDF.NumberOfPages; i++)
+                {
+                    oText.Append(PdfTextExtractor.GetTextFromPage(oPDF, i));
+                }
+                var oReturn = oText.ToString().Split(new char[] { '\n' }).ToList();
+                return oReturn;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfReaderEXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = PdfReaderClass.toString("File1.pdf");
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
